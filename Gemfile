4b6aa4ec09679ee267fa36a8948ec4d088553ff0
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-03-28 22:36:12 +0300

    ilk

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.2'
+gem 'execjs'
+gem 'therubyracer'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg', '0.12.2'
+end
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthDemo::Application.config.secret_token = '1c52b247fb40c2f3f0442cdd548a2713ffe61843142be74074abc6024bcbeee8a15f6a9879e4840a179f2f07496a0fc23ff0344132989f45ecfe230c87d803ae'

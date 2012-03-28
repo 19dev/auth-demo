15d9bf069f4893d02149bd81ee96ada39e2b6b54
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-03-29 01:12:39 +0300

    readme

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,20 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    if user.persisted?
+    if user.role? :geek or user.role? :guest
       can :read, Post
-      can :manage, Post, :user_id => user.id
-    else
-      can :read, Post
+    end
+
+    if user.role? :moderator
+      can [:read, :create], Post
+    end
+
+    if user.role? :admin
+      can [:read, :create, :update], Post
+    end
+
+    if user.role? :superadmin
+      can :manage, Post
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120328203360) do
+ActiveRecord::Schema.define(:version => 20120328214501) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(:version => 20120328203360) do
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                           :null => false
+    t.datetime "updated_at",                           :null => false
+    t.string   "role",            :default => "guest"
   end
 
 end

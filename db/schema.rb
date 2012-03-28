e59ed6d032c42c1f8829251bdf1dee82ede3b726
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-03-28 22:40:58 +0300

    scaffold: post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120328193701) do
+ActiveRecord::Schema.define(:version => 20120328193814) do
+
+  create_table "posts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
